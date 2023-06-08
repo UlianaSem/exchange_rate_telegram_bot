3c55c8a58dca438aaa5f4e4ac38d6f4ea5e5118e
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-08 20:56:50 +0500

    Update main.py - Update programm logic for telegram bot

--- src/main.py
+++ src/main.py
@@ -12,17 +12,30 @@ HEADERS = {
 }
 PATH_TO_FILE = 'src/rate_data.json'
 TOKEN = os.environ.get('EXCHANGE_RATE_BOT_API')
+HELP = """Приветствую! Вам доступно две команды:
+/help - для вывода этой справки,
+/get - для получения курса валюты относительно рубля. Команда вводится в формате "/get валюта". Например, /get USD"""
+
+bot = telebot.TeleBot(TOKEN)
 
 
-def main():
-    """Задает алгоритм работы программы"""
-    currency = input("Введите код валюты, курс которой хотите получить относительно рубля (например, USD)\n").strip()
+@bot.message_handler(commands=['help'])
+def get_help(message):
+    """Отправляет справку в чат пользователю
+    :param message: сообщение пользователя"""
+    bot.send_message(message.chat.id, HELP)
 
+
+@bot.message_handler(commands=['get'])
+def main(message):
+    """Отправляет курс в чат пользователю
+    :param message: сообщение пользователя"""
+    currency = src.utils.get_currency(message)
     rates_dictionary = src.utils.get_exchange_rate(URL, HEADERS, PARAMS)
     rate = src.utils.get_desired_currency(rates_dictionary, currency)
     answer_for_user = src.utils.check_in_file(currency, rate, rates_dictionary, PATH_TO_FILE)
-    print(answer_for_user)
+
+    bot.send_message(message.chat.id, answer_for_user)
 
 
-if __name__ == "__main__":
-    main()
+bot.polling(none_stop=True)
